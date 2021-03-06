7ff2d7c63dec820003d3e8b7e43e16c6af3f2b32
ele <2727262253@qq.com>
2021-03-06 14:51:00 +0800

    annotate

--- src/main/java/io/eele/collector/components/XmlReader.java
+++ src/main/java/io/eele/collector/components/XmlReader.java
@@ -14,6 +14,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+/**
+ * 根据表名解析 Mybatis Mapper XML 文件，获取 mapper 方法信息
+ */
 public class XmlReader {
 
 //    public static void main(String[] args) {
